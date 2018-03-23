2ff6dbfc4beef049270014ce72e06a12d539734d
ntrv <hydrangea56@me.com>
2018-03-24 04:29:06 +0900

    Split main handler

--- hubbot.go
+++ hubbot.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-
-	"github.com/apex/gateway"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/ntrv/hubbot/chatwork"
 	"gopkg.in/go-playground/webhooks.v3"
@@ -18,32 +12,4 @@ func main() {
 	hook.RegisterEvents(chatwork.HandlePullRequest, github.PullRequestEvent)
 	hook.RegisterEvents(chatwork.HandlePush, github.PushEvent)
 	lambda.Start(handleHubbot(webhooks.Handler(hook)))
-}
-
-type lambdaHandleFunc func(context.Context, events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
-
-func handleHubbot(h http.Handler) lambdaHandleFunc {
-	if h == nil {
-		h = http.DefaultServeMux
-	}
-	return func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-		r, err := gateway.NewRequest(ctx, req)
-		if err != nil {
-			fmt.Printf("%v\n", req)
-			return events.APIGatewayProxyResponse{}, err
-		}
-		w := gateway.NewResponse()
-		h.ServeHTTP(w, r)
-		resp := w.End()
-
-		if resp.StatusCode == 0 {
-			return events.APIGatewayProxyResponse{
-				StatusCode:      http.StatusOK,
-				Body:            resp.Body,
-				Headers:         resp.Headers,
-				IsBase64Encoded: resp.IsBase64Encoded,
-			}, nil
-		}
-		return resp, nil
-	}
 }
