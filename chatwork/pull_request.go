0002d4b0e67bddaf396307b9c60dd2a94f344386
ntrv <hydrangea56@me.com>
2018-03-24 23:35:59 +0900

    Fix type of ProcessPayloadFunc

--- chatwork/pull_request.go
+++ chatwork/pull_request.go
@@ -9,9 +9,9 @@ import (
 	"github.com/ntrv/webhooks/github"
 )
 
-func (c client) HandlePullRequest(payload interface{}, header webhooks.Header, w *http.ResponseWriter) {
+func (c client) HandlePullRequest(payload interface{}, header webhooks.Header, w http.ResponseWriter) {
 	pl := payload.(github.PullRequestPayload)
 	j, _ := json.Marshal(pl)
 	fmt.Printf("%v\n", string(j))
-	http.Error(*w, "Status OK", http.StatusOK)
+	http.Error(w, "Status OK", http.StatusOK)
 }
--- chatwork/push.go
+++ chatwork/push.go
@@ -9,9 +9,9 @@ import (
 	"github.com/ntrv/webhooks/github"
 )
 
-func (c client) HandlePush(payload interface{}, header webhooks.Header, w *http.ResponseWriter) {
+func (c client) HandlePush(payload interface{}, header webhooks.Header, w http.ResponseWriter) {
 	pl := payload.(github.PushPayload)
 	j, _ := json.Marshal(pl)
 	fmt.Printf("%v\n", string(j))
-	http.Error(*w, "Status OK", http.StatusOK)
+	http.Error(w, "Status OK", http.StatusOK)
 }
