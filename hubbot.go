57f292bf28b0b3880b10f21c2dac192a722a6d6c
ntrv <hydrangea56@me.com>
2018-05-24 03:29:05 +0900

    Use PostProcessFunc

--- hubbot.go
+++ hubbot.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	cw "github.com/griffin-stewie/go-chatwork"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/ntrv/hubbot/chatwork"
@@ -12,17 +13,29 @@ import (
 	gh "gopkg.in/go-playground/webhooks.v3/github"
 )
 
+func PostCWActionFunc(msg string, c echo.Context) error {
+	cli := cw.NewClient(os.Getenv("API_KEY"))
+	res, err := cli.PostRoomMessage(os.Getenv("ROOM_ID"), msg)
+	if err != nil {
+		return echo.NewHTTPError(
+			http.StatusInternalServerError,
+			err.Error(),
+		)
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func PrintActionFunc(msg string, c echo.Context) error {
+	return c.String(http.StatusOK, msg)
+}
+
 func main() {
 	hook := github.NewHook()
-	cw := chatwork.New(
-		&chatwork.Config{
-			ApiKey: os.Getenv("API_KEY"),
-			RoomId: os.Getenv("ROOM_ID"),
-		},
-	)
+	cw := chatwork.New()
 
-	hook.RegisterEvents(cw.HandlePush, gh.PushEvent)
-	hook.RegisterEvents(cw.HandlePullRequest, gh.PullRequestEvent)
+	hook.RegisterEvents(cw.HandlePush(PrintActionFunc), gh.PushEvent)
+	hook.RegisterEvents(cw.HandlePullRequest(PrintActionFunc), gh.PullRequestEvent)
 
 	e := echo.New()
 	e.Use(middleware.Logger())
