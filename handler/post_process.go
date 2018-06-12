53e5f5d48b0442626213f33b21268dc30aab57c0
ntrv <hydrangea56@me.com>
2018-06-12 23:36:02 +0900

    Decode base64 response

--- handler/post_process.go
+++ handler/post_process.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 	"os"
+	"encoding/base64"
 
 	chatwork "github.com/griffin-stewie/go-chatwork"
 	"github.com/labstack/echo"
@@ -12,14 +13,22 @@ type PostProcessFunc func(msg string, c echo.Context) error
 
 func SendChatworkPostProcess(msg string, c echo.Context) error {
 	cw := chatwork.NewClient(os.Getenv("API_KEY"))
-	res, err := cw.PostRoomMessage(os.Getenv("ROOM_ID"), msg)
+	res64, err := cw.PostRoomMessage(os.Getenv("ROOM_ID"), msg)
 	if err != nil {
 		return echo.NewHTTPError(
 			http.StatusInternalServerError,
 			err.Error(),
 		)
 	}
-	return c.JSON(http.StatusOK, res)
+
+	res, err := base64.URLEncoding.DecodeString(string(res64))
+	if err != nil {
+		return echo.NewHTTPError(
+			http.StatusInternalServerError,
+			err.Error(),
+		)
+	}
+	return c.JSON(http.StatusOK,  res)
 }
 
 func PrintPostProcess(msg string, c echo.Context) error {
